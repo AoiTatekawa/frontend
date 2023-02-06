277ee813cea199c53328fa10a8d6640136adf7f9
AoiTatekawa <tatekawa0509@gmail.com>
2023-02-06 23:35:46 +0900

    python_vue_create

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
-<template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+<!-- <template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-nav {
-  padding: 30px;
-}
+  data: () => ({
+    //
+  }),
+};
+</script> -->
+<script>
+export default {};
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<template>
+  <v-app>
+    <v-app-bar color="success" dark app>
+      <v-toolbar-title>求人情報</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn text to="/">ホーム</v-btn>
+        <v-btn text to="/editor">仕事追加</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-content>
+      <router-view />
+    </v-content>
+    <v-footer color="success" dark app class="justify-center">
+      Django Startup - Step3
+    </v-footer>
+  </v-app>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,64 @@
+// import Vue from "vue";
+// import VueRouter from "vue-router";
+// import HomeView from "../views/HomeView.vue";
+
+// Vue.use(VueRouter);
+
+// const routes = [
+//   {
+//     path: "/",
+//     name: "home",
+//     component: HomeView,
+//   },
+//   {
+//     path: "/about",
+//     name: "about",
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () =>
+//       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+//   },
+// ];
+
+// const router = new VueRouter({
+//   mode: "history",
+//   base: process.env.BASE_URL,
+//   routes,
+// });
+
+// export default router;
+
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "../views/HomeView.vue";
+import Job from "../views/Job.vue";
+import JobEditor from "../views/JobEditor.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "HomePage",
+    component: Home,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/job/:id", // 追加
+    name: "JobDetailPage",
+    component: Job,
+    props: true,
+  },
+  {
+    path: "/editor", //追加
+    name: "editor",
+    component: JobEditor,
+    props: true,
   },
 ];
 
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
   routes,
 });
 
